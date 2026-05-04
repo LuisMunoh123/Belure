324067dbbae99e7c9169a62c2201b1a373132cd1
LuisMunoh123 <luis.munoz.vasquez2003@gmail.com>
2026-05-04 17:19:07 -0300

    estructura base para merge sort listoco

---
+++ src/merge_sort.h
@@ -0,0 +1,7 @@
+#ifndef MERGE_SORT_H
+#define MERGE_SORT_H
+
+void merge_sort_classic(int arr[], int left, int right);
+void merge_sort_optimized(int arr[], int left, int right, int threshold);
+
+#endif
